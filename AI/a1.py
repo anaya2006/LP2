692cca1370f80d9f4a2f12947345f9eed642ae63
anaya2006 <anayabadave@gmail.com>
2026-05-11 16:17:43 +0530

    Add files via upload

---
+++ AI/a1.py
@@ -0,0 +1,72 @@
+import sys
+arr=[]
+def selection(n):
+   arr.clear() 
+   for i in range (0,n):
+       m=int(input(f"element {i+1} : "))
+       arr.append(m)
+   print("unsorted arr ",arr)
+   for i in range (0,n):
+      min_ind=i
+      for j in range (i+1,n):
+          if arr[j]<arr[min_ind]:
+              min_ind=j
+      arr[i],arr[min_ind]= arr[min_ind], arr[i]
+   print("sorted ",arr)
+ 
+#selection(5)
+labels=['A', 'B', 'C', 'D', 'E']          
+graph= [  [0,3,2,0,0],
+          [3,0,5,4,5],
+          [2,5,0,0,3],
+          [0,4,0,0,1],
+          [0,5,3,1,0]
+       ]         
+
+def prims(graph, V, labels):
+    selected=[False]*V
+    selected[0]=True
+    wt=0
+    edges=0
+    while edges<V-1:
+        minimum=sys.maxsize
+        x=0
+        y=0
+        for i in range (V):
+           if selected[i]:
+             for j in range (V):
+                if not selected[j] and graph[i][j]!=0:
+                     if minimum>graph[i][j]:
+                        minimum=graph[i][j]
+                        x=i
+                        y=j
+        print (f"{labels[x]}-{labels[y]} : {graph[x][y]}")
+        selected[y]=True
+        wt+=graph[x][y]
+        edges+=1
+    print("total cost of mst: ", wt)
+
+
+
+while True:
+    print("1. Selection Sort \n2. Prims Algorithm\n0. Exit")
+    print()
+    ch=int(input("Enter your choice : "))
+    
+    if ch==1:
+        n=int(input("Enter no of elements in array : "))
+        selection(n)
+        continue
+    
+    if ch==2:
+        prims(graph, 5, labels)
+        continue
+        
+    if ch==0:
+        print("Exiting..")
+        break
+        
+    else:
+        print("Please enter valid choice")
+        
+    
